9fdac9b70f1861a308ccf901a52fa347ec7193a2
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-15 03:15:01 -0230

    Remove unecessary ublue services

--- build_files/scripts/03-ublue-main.sh
+++ build_files/scripts/03-ublue-main.sh
@@ -3,6 +3,6 @@
 set -ouex pipefail
 
 dnf5 -y copr enable ublue-os/packages
-dnf5 -y install ublue-os-udev-rules ublue-os-update-services ublue-os-signing ublue-os-luks ublue-os-just
-dnf5 versionlock add ublue-os-udev-rules ublue-os-update-services ublue-os-signing ublue-os-luks ublue-os-just
+dnf5 -y install ublue-os-udev-rules ublue-os-update-services
+dnf5 versionlock add ublue-os-udev-rules ublue-os-update-services
 dnf5 -y copr disable ublue-os/packages
