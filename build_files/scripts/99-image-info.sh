23512efff28cf5cbfb5f3acd421f29ccc2e7e332
qkmaxware <qkmaxware@gmail.com>
2026-03-15 16:03:04 -0700

    Renamed custom logo file

--- build_files/scripts/99-image-info.sh
+++ build_files/scripts/99-image-info.sh
@@ -17,7 +17,7 @@ BASE_IMAGE_NAME="Kinoite $FEDORA_MAJOR_VERSION"
 
 # OS Release File
 mkdir -p /usr/share/icons/hicolor/scalable/
-cp ctx/files/usr/share/icons/hicolor/scalable/distro-logo.svg /usr/share/icons/hicolor/scalable/distro-logo.svg
+cp ctx/files/usr/share/icons/hicolor/scalable/callisto-logo.svg /usr/share/icons/hicolor/scalable/callisto-logo.svg
 sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
 sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=\"${IMAGE_PRETTY_NAME} (FROM Fedora ${BASE_IMAGE_NAME^})\"/" /usr/lib/os-release
 sed -i "s/^NAME=.*/NAME=\"$IMAGE_PRETTY_NAME\"/" /usr/lib/os-release
@@ -30,7 +30,7 @@ sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr
 sed -i "s/^ID=fedora/ID=${IMAGE_PRETTY_NAME,}\nID_LIKE=\"${IMAGE_LIKE}\"/" /usr/lib/os-release
 sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
 sed -i "s/^VERSION_CODENAME=.*/VERSION_CODENAME=${VERSION_CODENAME,,}/" /usr/lib/os-release
-sed -i 's/^LOGO=.*/LOGO=distro-logo/' /usr/lib/os-release
+sed -i 's/^LOGO=.*/LOGO=callisto-logo/' /usr/lib/os-release
 
 #if [[ -n "${SHA_HEAD_SHORT:-}" ]]; then
 #  echo "BUILD_ID=\"$SHA_HEAD_SHORT\"" >> /usr/lib/os-release
