0cc4c532fc99b69916feb33231258e5904ec1012
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-16 20:26:57 -0230

    Make script a correct shell script; fix missing / in path

--- build_files/scripts/08-configure-fastfetch.sh
+++ build_files/scripts/08-configure-fastfetch.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -ouex pipefail
+
 # Load in fastfetch configs
 mkdir -p /etc/fastfetch
 cp -r /ctx/files/etc/fastfetch/* /etc/fastfetch
--- build_files/scripts/99-image-info.sh
+++ build_files/scripts/99-image-info.sh
@@ -46,4 +46,4 @@ sed -i "s/^EFIDIR=.*/EFIDIR=\"fedora\"/" /usr/sbin/grub2-switch-to-blscfg
 # Add container image keys for image updating
 mkdir -p /usr/etc/pki/containers /usr/etc/containers
 cp -r /ctx/files/etc/containers/* /etc/containers
-cp -r /ctx/files/etc/pki/containers/* etc/pki/containers
+cp -r /ctx/files/etc/pki/containers/* /etc/pki/containers
