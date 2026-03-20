3a5fafe232fd779ce5e565ac34c69d9961f939fa
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-20 06:23:57 -0230

    Set Callisto theme as default theme

--- build_files/scripts/08-configure-plasma.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-# Set the default for new users to plasma dark
-kwriteconfig6 --file /etc/xdg/kdeglobals --group General --key ColorScheme BreezeDark
-kwriteconfig6 --file /etc/xdg/kdeglobals --group KDE --key LookAndFeelPackage org.kde.breezedark.desktop
--- build_files/scripts/97-theming.sh
+++ build_files/scripts/97-theming.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -ouex pipefail
 
@@ -8,3 +8,5 @@ dnf5 -y copr disable deltacopy/darkly
 
 mkdir -p /usr/share/plasma/look-and-feel/Callisto
 cp -rf /ctx/files/usr/share/plasma/look-and-feel/Callisto /usr/share/plasma/look-and-feel/
+
+kwriteconfig6 --file /etc/xdg/kdeglobals --group KDE --key LookAndFeelPackage Callisto
