b756aaaf97423a729b5da9aa0bb7c2b2635a9566
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-20 05:47:51 -0230

    Establish callisto theme

---
+++ build_files/files/usr/share/plasma/look-and-feel/Callisto/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,95 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1280x800": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "509",
+                            "popupWidth": "647"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        },
+                        "/Appearance": {
+                            "fontWeight": "400"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 3.4444444444444446,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 71.11111111111111,
+            "minimumLength": 71.11111111111111,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
--- build_files/scripts/97-theming.sh
+++ build_files/scripts/97-theming.sh
@@ -5,3 +5,6 @@ set -ouex pipefail
 dnf5 -y copr enable deltacopy/darkly
 dnf5 -y install darkly
 dnf5 -y copr disable deltacopy/darkly
+
+mkdir -p /usr/share/plasma/look-and-feel/Callisto
+cp -rf /ctx/files/usr/share/plasma/look-and-feel/Callisto /usr/share/plasma/look-and-feel/Callisto
