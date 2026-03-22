507a0c6509ba3c6756c91daecbb50d93e897b338
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-21 23:13:47 -0230

    Add the default background to the theme; add a preview to the theme

--- build_files/files/usr/share/plasma/look-and-feel/Callisto/contents/layouts/org.kde.plasma.desktop-layout.js
+++ build_files/files/usr/share/plasma/look-and-feel/Callisto/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,1 +1,8 @@
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
+
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+    desktopsArray[j].currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
+    desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/M42_Orions_Nebula_");
+}
