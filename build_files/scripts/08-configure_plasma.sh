6c23efa14884d03a4901b005f130b7e101420ced
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-24 16:59:38 -0230

    Install Kwin effect

--- build_files/scripts/08-configure_plasma.sh
+++ build_files/scripts/08-configure_plasma.sh
@@ -10,3 +10,4 @@ curl -L "https://github.com/peterfajdiga/kwin4_effect_geometry_change/releases/d
 | tar -xzC "/usr/share/kwin/effects/" --strip-components=1
 kwriteconfig6 --file /etc/xdg/kwinrc --group Effect-kwin4_effect_geometry_change --key Duration 500
 kwriteconfig6 --file /etc/xdg/kwinrc --group Plugins --key "kwin4_effect_geometry_changeEnabled" true
+kpackagetool6 --global --type=KWin/Effect -i /usr/share/kwin/effects/kwin4_effect_geometry_change
