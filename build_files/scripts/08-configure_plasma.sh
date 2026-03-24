34dff7a14f644d01928a6dd01ba87ef81ac21754
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-24 13:47:31 -0230

    Download geometry change kwin effect, configure it, and enable it by default

--- build_files/scripts/08-configure_plasma.sh
+++ build_files/scripts/08-configure_plasma.sh
@@ -3,3 +3,10 @@
 set -ouex pipefail
 
 kwriteconfig6 --file /etc/xdg/kdeglobals --group KDE --key AnimationDurationFactor 0.35
+
+# Download geometry change kwin effect, configure it, and enable it by default 
+geometry_change_ver=1.5
+curl -L "https://github.com/peterfajdiga/kwin4_effect_geometry_change/releases/download/v${geometry_change_ver}/kwin4_effect_geometry_change_${geometry_change_ver//./_}.tar.gz" \
+| tar -xzC "/usr/share/kwin/effects/kwin4_effect_geometry_change" --strip-components=1
+kwriteconfig6 --file /etc/xdg/kwinrc --group Effect-kwin4_effect_geometry_change --key Duration 500
+kwriteconfig6 --file /etc/xdg/kwinrc --group Plugins --key "kwin4_effect_geometry_changeEnabled" true
