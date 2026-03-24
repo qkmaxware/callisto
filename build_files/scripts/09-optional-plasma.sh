e79bc6e0f73c8a92ca86e9dd1da94ae59d3a52a3
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-24 03:20:28 -0230

    Register the kwin script

--- build_files/scripts/09-optional-plasma.sh
+++ build_files/scripts/09-optional-plasma.sh
@@ -6,4 +6,6 @@ set -ouex pipefail
 dnf5 -y install https://github.com/xarbit/plasma6-applet-appgrid/releases/download/v1.7.4/plasma-applet-appgrid-1.7.4-1.fc43.x86_64.rpm
 
 # Add the fluid tile autotiling kwin script
-curl -L -o /usr/share/kwin/scripts/fluid-tile-v7.2.kwinscript https://codeberg.org/Serroda/fluid-tile/releases/download/v7.2/fluid-tile-v7.2.kwinscript
+fluid_tile_ver=7.2
+curl -L -o "/usr/share/kwin/scripts/fluid-tile-v${fluid_tile_ver}.kwinscript" "https://codeberg.org/Serroda/fluid-tile/releases/download/v${fluid_tile_ver}/fluid-tile-v${fluid_tile_ver}.kwinscript"
+plasmapkg2 -t kwinscript -i "/usr/share/kwin/scripts/fluid-tile-v${fluid_tile_ver}.kwinscript"
