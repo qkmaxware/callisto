503686d5359ec0cd326254f763a6d03b47c75a2a
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-24 17:43:12 -0230

    Add missing directory

--- build_files/scripts/08-configure_plasma.sh
+++ build_files/scripts/08-configure_plasma.sh
@@ -4,6 +4,8 @@ set -ouex pipefail
 
 kwriteconfig6 --file /etc/xdg/kdeglobals --group KDE --key AnimationDurationFactor 0.35
 
+mkdir /tmp/effects
+
 # Download geometry change kwin effect, configure it, and enable it by default 
 geometry_change_ver=1.5
 curl -L "https://github.com/peterfajdiga/kwin4_effect_geometry_change/releases/download/v${geometry_change_ver}/kwin4_effect_geometry_change_${geometry_change_ver//./_}.tar.gz" \
@@ -11,4 +13,5 @@ curl -L "https://github.com/peterfajdiga/kwin4_effect_geometry_change/releases/d
 kwriteconfig6 --file /etc/xdg/kwinrc --group Effect-kwin4_effect_geometry_change --key Duration 500
 kwriteconfig6 --file /etc/xdg/kwinrc --group Plugins --key "kwin4_effect_geometry_changeEnabled" true
 kpackagetool6 --global --type=KWin/Effect -i /tmp/effects/kwin4_effect_geometry_change
+
 rm -rf /tmp/effects
