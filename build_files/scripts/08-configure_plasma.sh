b62dd6ffaba5748aecd4cfbf3792c21b71dda4f8
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-03-24 22:54:37 -0230

    Install the Squash-Plus kwin effect, configure it, and enable it by default

--- build_files/scripts/08-configure_plasma.sh
+++ build_files/scripts/08-configure_plasma.sh
@@ -14,4 +14,13 @@ kwriteconfig6 --file /etc/xdg/kwinrc --group Effect-kwin4_effect_geometry_change
 kwriteconfig6 --file /etc/xdg/kwinrc --group Plugins --key "kwin4_effect_geometry_changeEnabled" true
 kpackagetool6 --global --type=KWin/Effect -i /tmp/effects/kwin4_effect_geometry_change
 
+# Install the Squash-Plus kwin effect, configure it, and enable it by default
+git clone --depth 1 https://github.com/Shaurya-Kalia/Squash-Plus.git /tmp/effects/Squash-Plus
+
+kwriteconfig6 --file /etc/xdg/kwinrc --group "Effect-kwin4_effect_squashplus" --group "General" --key Duration 1000
+kwriteconfig6 --file /etc/xdg/kwinrc --group "Effect-kwin4_effect_squashplus" --group "General" --key Opacity 80
+kwriteconfig6 --file /etc/xdg/kwinrc --group Plugins --key "kwin4_effect_squashplusEnabled" true
+
+kpackagetool6 --global --type=KWin/Effect -i /tmp/effects/Squash-Plus
+
 rm -rf /tmp/effects
