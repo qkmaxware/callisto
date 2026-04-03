b1b0808153eea866aa652d5420b07e052d804048
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2026-04-03 16:22:11 -0230

    Update vermouth to V1.0.0

--- build_files/scripts/05-default-packages_and_flatpak.sh
+++ build_files/scripts/05-default-packages_and_flatpak.sh
@@ -63,7 +63,7 @@ dnf5 -y install umu-launcher \
     --best
 dnf5 -y remove nobara-gpg-keys
 
-vermouth_ver=0.7.1
+vermouth_ver=1.0.0
 dnf5 -y install "https://github.com/dekomote/vermouth/releases/download/v${vermouth_ver}/vermouth-${vermouth_ver}-x86_64.rpm"
 
 # Add Flathub to the image for eventual application
