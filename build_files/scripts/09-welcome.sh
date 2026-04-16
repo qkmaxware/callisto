24811774d0c43631e001107fa513d25ba97abd8b
Colin <qkmaxware@users.noreply.github.com>
2026-04-16 10:29:43 -0700

    Update 09-welcome.sh

--- build_files/scripts/09-welcome.sh
+++ build_files/scripts/09-welcome.sh
@@ -6,3 +6,4 @@ mkdir -p /usr/share/plasma/plasma-welcome/
 cp -rf /ctx/files/usr/share/plasma/plasma-welcome/. /usr/share/plasma/plasma-welcome/
 
 chmod +x /usr/share/plasma/plasma-welcome/extra-pages/workflows/*.sh
+chmod +x /usr/share/plasma/plasma-welcome/extra-pages/workflows/*.desktop
