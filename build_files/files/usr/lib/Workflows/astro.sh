246ab7e19b0219f3241fd826c76c8fc33ec10325
qkmaxware <qkmaxware@gmail.com>
2026-04-22 18:01:50 -0700

    Removed failed welcome pages experiment

--- build_files/files/usr/lib/Workflows/astro.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-flatpak install --assumeyes flathub org.kde.kstars
-flatpak install --assumeyes flathub org.siril.Siril
-flatpak install --assumeyes flathub net.oz9aec.Gpredict
--- build_files/files/usr/lib/Workflows/creative.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-flatpak install --assumeyes flathub org.blender.Blender
-flatpak install --assumeyes flathub org.kde.krita
-flatpak install --assumeyes flathub org.kde.kdenlive
-flatpak install --assumeyes flathub org.gimp.GIMP
-flatpak install --assumeyes flathub org.inkscape.Inkscape
-flatpak install --assumeyes flathub org.audacityteam.Audacity
