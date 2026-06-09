2b99f2e820d64277e42735acba9de98bb2f8a899
qkmaxware <qkmaxware@gmail.com>
2026-06-08 18:47:03 -0700

    A lot of changes with theming being moved to packages

--- build_files/scripts/09-theming.sh
+++ build_files/scripts/09-theming.sh
@@ -11,6 +11,7 @@ releasever=$(rpm -E '%fedora')
 dnf5 -y install darkly \
   --repofrompath='darkly,https://download.copr.fedorainfracloud.org/results/deltacopy/darkly/fedora-$releasever-x86_64/' \
   --setopt="darkly.gpgkey=https://download.copr.fedorainfracloud.org/results/deltacopy/darkly/pubkey.gpg"
+dnf5 -y install callisto-theme-colour-scheme --repo callisto # And it's custom colour scheme
 
 # Install tela icon theme
 git clone --depth 1 https://github.com/vinceliuice/Tela-icon-theme.git
@@ -20,17 +21,11 @@ rm -rf Tela-icon-theme
 # Overwrite tela's start-here logo with callisto
 for dir in '/usr/share/icons/Tela (Callisto)/32/status/' '/usr/share/icons/Tela (Callisto)/24/panel/' '/usr/share/icons/Tela (Callisto)/22/panel/' '/usr/share/icons/Tela (Callisto)/16/panel/'; do sudo cp -f ctx/files/usr/share/icons/hicolor/scalable/start-here.svg "$dir"; done
 
-# Install Darkly Callisto color theme
-mkdir -p /usr/share/color-schemes
-cp -f ctx/files/usr/share/color-schemes/DarklyCallisto.colors /usr/share/color-schemes
-
 # Add the Callisto SDDM theme
 dnf5 -y install callisto-theme-sddm --repo callisto
 
 # Add the Callisto theme
-mkdir -p /usr/share/plasma/look-and-feel/Callisto
-cp -rf /ctx/files/usr/share/plasma/look-and-feel/Callisto /usr/share/plasma/look-and-feel/
-
+dnf5 -y install callisto-theme --repo callisto
 kwriteconfig6 --file /etc/xdg/kdeglobals --group KDE --key LookAndFeelPackage Callisto
 
 # Rip out default installed wallpapers (minus a few special ones)
--- build_files/scripts/97-branding.sh
+++ build_files/scripts/97-branding.sh
@@ -2,18 +2,6 @@
 
 set -ouex pipefail
 
-# Change Fedora logo to Callisto Logo
-mkdir -p /usr/share/icons/hicolor/scalable/apps
-cp -f ctx/files/usr/share/icons/hicolor/scalable/callisto-logo.svg /usr/share/icons/hicolor/scalable/apps/distribution-logo.svg
-cp -f ctx/files/usr/share/icons/hicolor/scalable/callisto-logo.svg /usr/share/icons/hicolor/scalable/apps/callisto-logo.svg
-cp -f ctx/files/usr/share/icons/hicolor/scalable/callisto-logo.svg /usr/share/icons/hicolor/scalable/apps/fedora-logo-icon.svg
-cp -f ctx/files/usr/share/icons/hicolor/scalable/callisto-logo.svg /usr/share/icons/hicolor/scalable/apps/fedora-logo-sprite.svg
-
-mkdir -p /usr/share/pixmaps
-cp -f ctx/files/usr/share/pixmaps/callisto-logo.png /usr/share/pixmaps/fedora-logo.png
-cp -f ctx/files/usr/share/pixmaps/callisto-logo.png /usr/share/pixmaps/system-logo-white.png
-cp -f ctx/files/usr/share/pixmaps/callisto-logo-small.png /usr/share/pixmaps/fedora-logo-small.png
-
 # Change "start-here" application launcher icon to a modified logo
 cp -f ctx/files/usr/share/icons/hicolor/scalable/start-here.svg /usr/share/icons/breeze-dark/places/16/start-here-kde-symbolic.svg
 cp -f ctx/files/usr/share/icons/hicolor/scalable/start-here.svg /usr/share/icons/breeze-dark/places/22/start-here-kde-symbolic.svg
